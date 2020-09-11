c113c4725f28cf543a18878ffc0fbc547c36fa87
Kreams <40688477+KreamsIce@users.noreply.github.com>
2020-09-12 01:26:37 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kreams#0885'
+description 'Coronavirus'
+version '1.0.0'
+
+server_scripts {
+	'config.lua',
+	'server.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
